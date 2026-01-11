4f0b2e47300e30466ea56e285253f28f4b527175
tiagordomingues <tiagordomingues@gmail.com>
2026-01-11 18:36:51 +0000

    Delete 1_hello_world directory

--- 1_hello_world/file_list_gen.cmake
+++
@@ -1,7 +0,0 @@
-list(
-  APPEND
-  PROJECT_SOURCES
-  ${CMAKE_CURRENT_LIST_DIR}/fonts/font_medium_data.c
-  ${CMAKE_CURRENT_LIST_DIR}/screens/screen_hello_world_gen.c
-  ${CMAKE_CURRENT_LIST_DIR}/ui_hello_world_gen.c
-  ${CMAKE_CURRENT_LIST_DIR}/ui_hello_world.c)
